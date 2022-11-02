fbc7fa4c10fd9f49d8478a13e8d90b0ef6808770
flipeprez <flipeprez@gmail.com>
2022-11-02 16:19:27 -0300

    add routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+//add routes
+app.get('/', (req, res) => {
+    res.send("F1 API DATA")
+});
+
 app.listen(PORT, () => {
     console.log(`server is running in http://localhost:${PORT}`);
 });
