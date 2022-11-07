1d0228049ecb2778d236cfee7de2f5d42848152e
flipeprez <flipeprez@gmail.com>
2022-11-07 17:21:22 -0300

    add two new end points

--- src/routes/teams.js
+++ src/routes/teams.js
@@ -12,4 +12,23 @@ router.post('/teams', (req, res)=>{
         .catch((err) => res.json({err}));
 });
 
+//get all teamdata with .get()
+router.get('/teams', (req, res)=>{
+    //here we are going to find de f1 teams with .find()
+    teamschema 
+        .find()
+        .then((data) => res.json(data))
+        .catch((err) => res.json({err}));
+});
+
+//get one teamdata with .get()
+router.get('/teams/:id', (req, res)=>{
+    //here we are going to find de f1 teams with .findById()
+    const {id} = req.params;
+    teamschema 
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((err) => res.json({err}));
+});
+
 module.exports = router;
